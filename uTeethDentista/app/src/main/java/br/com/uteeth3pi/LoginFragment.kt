89027cdc99c5d21ace39e31d7b7231295636f563
Possanii <gpossanii@outlook.com>
2023-04-26 22:30:41 -0300

    change path name

---
+++ uTeethDentista/app/src/main/java/br/com/uteeth3pi/LoginFragment.kt
@@ -0,0 +1,67 @@
+package br.com.uteeth3pi
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.uteeth3pi.databinding.FragmentLoginBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginFragment : AppCompatActivity() {
+
+    private lateinit var binding: FragmentLoginBinding
+    private val auth = FirebaseAuth.getInstance()
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        supportActionBar?.hide()
+        window.statusBarColor= Color.parseColor("#FFFFFFFF")
+
+        binding.btnSignIn.setOnClickListener{view ->
+            val email = binding.etEmailLogin.text.toString()
+            val senha = binding.etPasswordLogin.text.toString()
+
+            if (email.isEmpty() || senha.isEmpty()){
+                val snackbar = Snackbar.make(view, "Preencha todos os campos!", Snackbar.LENGTH_SHORT)
+                snackbar.setBackgroundTint(Color.RED)
+                snackbar.show()
+            }else{
+                auth.signInWithEmailAndPassword(email, senha).addOnCompleteListener{autenticacao ->
+                    if (autenticacao.isSuccessful){
+                        navegarHome()
+                    }
+
+                }
+
+            }
+        }
+
+        }
+        private fun navegarHome(){
+        val intent = Intent(this,HomeFragment::class.java)
+        startActivity(intent)
+        finish()
+
+
+        binding.btnSignUp.setOnClickListener {
+            val intent = Intent(this, CriarContaFragment::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+    }
+
+
+
+
+
+}
